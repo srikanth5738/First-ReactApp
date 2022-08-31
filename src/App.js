cee68960e284176a02855b3bba2841db136a4a45
srikanth5738 <s.kotamarthi@iveond.com>
2022-08-31 10:54:37 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React from "react";
-import "./App.css";
+import "./styles.css";
+
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Events from "./pages/events";
-import AnnualReport from "./pages/annual";
-import Sri3 from "./pages/sri3";
-import Blogs from "./pages/blogs";
-import Sri2 from "./pages/sri2";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// PAGES
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Reports from "./pages/Reports";
+
+export default function App() {
   return (
-    <div className="apple">
-	
-		 <BrowserRouter>
-      <Navbar />
-      <Routes>
-        
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/annual" element={<AnnualReport />} />
-        <Route path="/sri3" element={<Sri3 />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/sri2" element={<Sri2 />} />
-        
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={Home} />
+          <Route path='/products' element={Products} />
+          <Route path='/reports' element={Reports} />
+        </Routes>
+      </Router>
     </div>
-
-	
-   
   );
 }
-
-export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const About = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		
-	}}
-	>
-	<h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
-	</div>
+const Home = () => (
+  <div>
+    <h1 className="title is-1">This is the Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
+      risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
+      nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
+      lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
+    </p>
+  </div>
 );
-};
 
-export default About;
+export default Home;
